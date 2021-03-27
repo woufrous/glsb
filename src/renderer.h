89a7e6ce78b8e81d73e673641afb721f62be1c65
Hans Grob <H.Grob@ifndef.de>
2021-03-27 07:51:25 +0100

    renderer: enable back face culling

--- src/renderer.h
+++ src/renderer.h
@@ -30,6 +30,8 @@ class Renderer {
             glEnable(GL_BLEND);
             glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
             glEnable(GL_DEPTH_TEST);
+            glCullFace(GL_BACK);
+            glEnable(GL_CULL_FACE);
         }
 
         void cleanup() {}
